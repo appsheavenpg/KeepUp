716cd5df29f96010ce4bbf5a5d894bec9db02210
Paweł Godlewski <appsheavenpg@gmail.com>
2024-09-30 08:05:44 +0200

    refactor: Refactored MongoDatabaseTest.kt to use testcontainer instead of mockk, refactored Todo to use _id to match MongoDb requirements

--- server/src/main/kotlin/com/appsheaven/keepup/todos/Todo.kt
+++ server/src/main/kotlin/com/appsheaven/keepup/todos/Todo.kt
@@ -3,7 +3,7 @@ package com.appsheaven.keepup.todos
 import org.bson.types.ObjectId
 
 internal data class Todo(
-    val id: ObjectId,
+    @Suppress("PropertyName") val _id: ObjectId,
     val title: String,
     val description: String,
     val completed: Boolean
