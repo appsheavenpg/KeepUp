291a3992125721963f0d95d1f1cfcbbc8939593b
appsheavenpg <75860066+appsheavenpg@users.noreply.github.com>
2024-10-01 23:31:49 +0200

    Merge pull request #13
    * feat: Added putTodo database operation

--- server/src/main/kotlin/com/appsheaven/keepup/core/KeepUpDatabase.kt
+++ server/src/main/kotlin/com/appsheaven/keepup/core/KeepUpDatabase.kt
@@ -3,9 +3,11 @@ package com.appsheaven.keepup.core
 import com.appsheaven.keepup.todos.Todo
 import com.mongodb.MongoException
 import com.mongodb.client.result.InsertOneResult
+import com.mongodb.client.result.UpdateResult
 
 internal interface KeepUpDatabase {
     @Throws(MongoException::class)
     suspend fun insertTodo(todo: Todo): InsertOneResult
     suspend fun getTodos(page: Int, pageSize: Int): List<Todo>
+    suspend fun putTodo(todo: Todo): UpdateResult
 }
