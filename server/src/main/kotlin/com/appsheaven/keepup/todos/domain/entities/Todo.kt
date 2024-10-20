b8201126ce0d1818d7e70ab7e2b1da0634f10412
Paweł Godlewski <appsheavenpg@gmail.com>
2024-10-20 13:13:21 +0200

    feat: Added mapping methods to Todo.kt

--- server/src/main/kotlin/com/appsheaven/keepup/todos/domain/entities/Todo.kt
+++ server/src/main/kotlin/com/appsheaven/keepup/todos/domain/entities/Todo.kt
@@ -1,10 +1,25 @@
 package com.appsheaven.keepup.todos.domain.entities
 
 import org.bson.types.ObjectId
+import com.appsheaven.keepup.todo.data.models.NetworkTodo
 
 internal data class Todo(
     @Suppress("PropertyName") val _id: ObjectId,
     val title: String,
     val description: String,
     val completed: Boolean
+)
+
+internal fun Todo.toNetworkTodo() = NetworkTodo(
+    id = _id.toHexString(),
+    title = title,
+    description = description,
+    completed = completed
+)
+
+internal fun NetworkTodo.toTodo() = Todo(
+    _id = ObjectId(id),
+    title = title,
+    description = description,
+    completed = completed
 )
